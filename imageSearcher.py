bf13bd2dd72e7a7e23eff07bb6c1a690822a7adc
ZevCe <cellaz@rpi.edu>
2024-09-04 17:53:42 -0400

    Refactor of image searcher and ability to write notes for an image

--- imageSearcher.py
+++ imageSearcher.py
@@ -2,6 +2,7 @@ from PIL import ImageTk, Image
 import tkinter as tk
 import json
 import utilFunctions
+import os
 
 
 class mainWindow():
@@ -19,7 +20,6 @@ class mainWindow():
         #basic defines for root window
         root = tk.Tk()
         root.title("OCR searcher")
-        root.geometry('400x250')
 
         #instructions for user
         instructions = tk.Label(root, text="enter key words you want to search by seperated with spaces\nonly images which contain all key words will be shown")
@@ -46,7 +46,7 @@ class mainWindow():
         for i in range(len(keyWords)):
             #checking that keyword is searchable
             if keyWords[i] in self.toDict:
-                #adding all relevant images to array
+                #adding all relevant images to map
                 for im in self.toDict[keyWords[i]]:
                     if im not in unfilteredResults:
                         unfilteredResults[im] = 1
@@ -75,7 +75,6 @@ class errorWindow():
     def __init__(self, kw):
         err = tk.Toplevel()
         err.title("error")
-        err.geometry("400x200")
         s = "Error: " + str(kw)
         explanation = tk.Label(err, text=s)
         explanation.pack()
@@ -85,68 +84,114 @@ class searchWindow():
     def __init__(self, filteredResults):
         res = tk.Toplevel()
         res.title("Results of search")
-        res.geometry("400x400")
     
-
         #for keeping track of which image we are looking at
         self.index = 0
-        
-        #displaying first image
-        self.img = ImageTk.PhotoImage(Image.open(filteredResults[self.index]))
-        self.display = tk.Label(res, image = self.img)
-        self.display.pack()
-        
-        #gui element to tell us index of image
-        s = "Showing image: " + str(self.index+1) + "/" + str(len(filteredResults)), "(" + filteredResults[self.index] + ")"
-        self.totalIM = tk.Label(res, text=s)
-        self.totalIM.pack(side=tk.BOTTOM)
-        
+
         #important for button functions
         self.filteredResults = filteredResults
 
-        #buttons for controlling which image is displayed
-        backBtn = tk.Button(res, text = "previous image", command=self.prevImage)
-        backBtn.pack(side=tk.LEFT)
+        #ui element for displaying the image and a global variable to track the image 
+        self.img = None
+        self.display = tk.Label(res)
+        self.display.pack()
         
-        fwdBtn = tk.Button(res, text = "next image", command=self.nextImage)
-        fwdBtn.pack(side=tk.RIGHT)
+        #gui element to tell us index of image
+        self.totalIM = tk.Label(res)
+        self.totalIM.pack(side=tk.BOTTOM)
+    
+        #button to save notes
+        saveNotesBtn = tk.Button(res, text = "save notes", command=self.saveNotes)
+        saveNotesBtn.pack(side=tk.BOTTOM)
+
+        #text widget for taking notes on current image
+        self.notesWindow = tk.Text(res)
+        self.notesWindow.pack(side=tk.BOTTOM)
+
+        #if we have more than one search result add buttons for navigating between them
+        if(len(self.filteredResults) > 1):
+            fwdBtn = tk.Button(res, text = "next image", command=self.nextImage)
+            fwdBtn.pack(side=tk.RIGHT)
+
+            backBtn = tk.Button(res, text = "previous image", command=self.prevImage)
+            backBtn.pack(side=tk.LEFT)
+
+        #filling in all our ui elements with data
+        self.updateDisplay()
 
         res.mainloop()
 
-    #controls for sorting thru images    
+    #controls for sorting thru images, just updates current index then calls update display function    
     def prevImage(self):
         #safeguards against moving out of bounds
         if self.index*-1 == len(self.filteredResults) -1:
             self.index = 0
         else:
             self.index -= 1
-            
-        #opening image and updating our label
-        self.img = ImageTk.PhotoImage(Image.open(self.filteredResults[self.index]))
-        self.display.config(image=self.img)
-        #determing which index to display to user
-        displayIndex = self.index
-        if displayIndex < 0:
-            displayIndex = len(self.filteredResults) + displayIndex
-        #giving user information about the search results
-        s = "Showing image: " + str(displayIndex+1) + "/" + str(len(self.filteredResults)), "(" + self.filteredResults[self.index] + ")"
-        self.totalIM.config(text=s)
+        
+        self.updateDisplay()
 
     def nextImage(self):
+        #safeguards against moving out of bounds
         if self.index == len(self.filteredResults) - 1:
             self.index = 0
         else:
             self.index += 1
 
-        self.img = ImageTk.PhotoImage(Image.open(self.filteredResults[self.index]))
+        self.updateDisplay()
+
+    #save notes from textbox to a file
+    def saveNotes(self):
+        #opening the file, writing to the file the new notes then closing the file
+        file = open("./image_notes/" + self.filteredResults[self.index][17:-4] + ".txt", "w")
+        file.write(self.notesWindow.get("1.0", "end"))
+        file.close()
+
+    #updates all elements of our display each time we
+    def updateDisplay(self):
+        #updating the image
+        self.updateImage()
+
+        #updating the notes to match the image
+        self.updateNotes()
+
+        #updating the image meta data
+        self.updateMetaData()
+
+
+    #updating the image being displayed based on current index
+    def updateImage(self):
+        #opening image
+        unsizedImage = Image.open(self.filteredResults[self.index])
+        
+        #halving size of image if its too big
+        if(unsizedImage.width > 1000):
+            unsizedImage = unsizedImage.resize((int(unsizedImage.width/2), int(unsizedImage.height/2)))
+        
+        #displaying image
+        self.img = ImageTk.PhotoImage(unsizedImage)
         self.display.config(image=self.img)
+
+    #upates the notes to be that of the current image
+    def updateNotes(self):
+        #checking if notes already exist for given image
+        fName = "./image_notes/" + self.filteredResults[self.index][17:-4] + ".txt"
+        if os.path.isfile(fName):
+            #if file exists read its contents into the notes box
+            file = open(fName, "r")
+            self.notesWindow.delete("1.0", "end")
+            self.notesWindow.insert("end", file.read())
+            file.close
+
+    #updating the meta data such as where image is stored and what number search result it is
+    def updateMetaData(self):
+        #determing which index to display to user
         displayIndex = self.index
         if displayIndex < 0:
             displayIndex = len(self.filteredResults) + displayIndex
-        s = "Showing image: " + str(displayIndex+1) + "/" + str(len(self.filteredResults)), "(" + self.filteredResults[self.index] + ")"
+        #giving user information about the search results
+        s = "Showing image: " + str(displayIndex+1) + "/" + str(len(self.filteredResults)), self.filteredResults[self.index]
         self.totalIM.config(text=s)
-
-
 
 
 mainWindow()
